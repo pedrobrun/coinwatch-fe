2691a0eb8031fa5acce20c64bedd1c0b48c50aee
Pedro Brun <pedroluccavb@gmail.com>
2022-05-24 11:53:07 -0300

    create myCoins page

--- pages/MyCoins.tsx
+++ pages/MyCoins.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import BackIcon from '../components/BackIcon';
 import ProtectedRoute from '../components/ProtectedRoute';
 import JwtCookieProvider from '../providers/JwtCookieProvider';
+import useJwtCookie from '../useContext/useJwtCookie';
 
 export default function MyCoins() {
+  const [myCoins, setMyCoins] = useState<any>();
+
+  const user = useJwtCookie();
+
+  const getMyCoins = async () => {
+    if (!user?.jwt) return;
+    const res = await axios.get('http://localhost:3000/coin/myCoins', {
+      headers: {
+        authorization: `Bearer ${user?.jwt}`,
+      },
+    });
+    setMyCoins(res.data.myCoins);
+  };
+
+  useEffect(() => {
+    getMyCoins();
+  });
+
   return (
     <JwtCookieProvider>
       <ProtectedRoute>
-        <div>MyCoins</div>
+        <BackIcon></BackIcon>
+
+        {myCoins &&
+          myCoins.map((c: any) => {
+            return (
+              <ul key={c.id}>
+                <li>{c.currency}</li>
+                <li>{c.name}</li>
+                <li>{c.price}</li>
+                <li>{c.logo_url}</li>
+                <li>{c.symbol}</li>
+              </ul>
+            );
+          })}
       </ProtectedRoute>
     </JwtCookieProvider>
   );
