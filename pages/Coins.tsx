c0931a6d617bbc47c8a952503b9fd3fea7bef0b0
Pedro Brun <pedroluccavb@gmail.com>
2022-05-24 10:49:27 -0300

    coins page

--- pages/Coins.tsx
+++ pages/Coins.tsx
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import { InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { toast } from 'react-toastify';
 import styles from '../styles/Coins.module.scss';
 
 function Coins({
@@ -10,6 +12,48 @@ function Coins({
 
   const handleBack = () => {
     router.back();
+  };
+
+  const addCoinToFavourites = async (coin: any) => {
+    const addedCoin = await axios.post(
+      'http://localhost:3000/coin',
+      {
+        coin,
+        username: 'First_username',
+      },
+      {
+        headers: {
+          authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkZpcnN0X3VzZXJuYW1lIiwiZW5jb2RlZFBhc3N3b3JkIjoiJDJiJDEyJGlTajhkbWZLb2ZSWWQxY05UUUx6cnVRNmMuWnR2MGwwNDB5Rk5sNjdNMWFzYUVuMFJRVU1HIiwiaWF0IjoxNjUzMzQyMTY2LCJleHAiOjE2NTMzNDM5NjZ9.7ms3TMgjhRIQ5lJyLWzYpLfoPpSYwFi4lLQaWOpIZXk',
+        },
+      }
+    );
+
+    if (addedCoin.data === 'Already favourite.') {
+      toast('Coin already favourited! 🙂', {
+        position: 'bottom-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+
+      return;
+    }
+
+    if (addedCoin.status === 200) {
+      toast.success('Coin successfully favourited!', {
+        position: 'bottom-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   };
 
   return (
@@ -43,6 +87,14 @@ function Coins({
                   </div>
 
                   <img className={styles.coinLogo} src={c.logo_url}></img>
+
+                  <img
+                    className={styles.addIcon}
+                    src="add-icon.webp"
+                    onClick={() => {
+                      addCoinToFavourites(c);
+                    }}
+                  ></img>
                 </li>
               </ul>
             );
@@ -56,9 +108,8 @@ function Coins({
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(`http://localhost:3000/api/currencies`);
+  const res = await fetch(`http://localhost:3001/api/currencies`);
   const coins = await res.json();
-  console.log(coins);
 
   if (!res) {
     return {
