8faa34ff42f018b18ac6f0440ef41fba9c3676f0
Pedro Brun <pedroluccavb@gmail.com>
2022-05-24 23:50:48 -0300

    improve my coins page

--- pages/MyCoins.tsx
+++ pages/MyCoins.tsx
@@ -4,6 +4,7 @@ import BackIcon from '../components/BackIcon';
 import ProtectedRoute from '../components/ProtectedRoute';
 import JwtCookieProvider from '../providers/JwtCookieProvider';
 import useJwtCookie from '../useContext/useJwtCookie';
+import styles from '../styles/MyCoins.module.scss';
 
 export default function MyCoins() {
   const [myCoins, setMyCoins] = useState<any>();
@@ -24,23 +25,99 @@ export default function MyCoins() {
     getMyCoins();
   });
 
+  const [clicked, setClicked] = useState(0);
+
+  const handleToggle = (index: number) => {
+    if (clicked === index) {
+      return setClicked(-1);
+    }
+    return setClicked(index);
+  };
+
   return (
     <JwtCookieProvider>
       <ProtectedRoute>
-        <BackIcon></BackIcon>
+        <div className={styles.wrapper}>
+          <BackIcon></BackIcon>
 
-        {myCoins &&
-          myCoins.map((c: any) => {
-            return (
-              <ul key={c.id}>
-                <li>{c.currency}</li>
-                <li>{c.name}</li>
-                <li>{c.price}</li>
-                <li>{c.logo_url}</li>
-                <li>{c.symbol}</li>
-              </ul>
-            );
-          })}
+          {myCoins &&
+            myCoins.map((c: any, index: number) => (
+              <div key={c.id} className={styles.coinContainer}>
+                <ul className={styles.coinCard}>
+                  <li>{c.currency}</li>
+                  <li>{c.name}</li>
+                  <li>${c.price}</li>
+                  <img src={c.logo_url} className={styles.logo}></img>
+                </ul>
+                {clicked === index && (
+                  <>
+                    <div className={styles.moreDetails}>
+                      <div>Status: {c.status}</div>
+                      <div className={styles.rightColumnItem}>
+                        Rank: {c.rank}
+                      </div>
+                      <div>
+                        Variation from yesterday:{' '}
+                        <span
+                          className={
+                            c.ytd.price_change_pct > 0
+                              ? styles.positive
+                              : styles.negative
+                          }
+                        >
+                          {c.ytd.price_change_pct}
+                        </span>
+                      </div>
+                      <div className={styles.rightColumnItem}>
+                        Variation from 1 week:{' '}
+                        <span
+                          className={
+                            c['7d'].price_change_pct > 0
+                              ? styles.positive
+                              : styles.negative
+                          }
+                        >
+                          {c['7d'].price_change_pct}
+                        </span>
+                      </div>
+                      <div>
+                        Variation from 1 month:{' '}
+                        <span
+                          className={
+                            c['30d'].price_change_pct > 0
+                              ? styles.positive
+                              : styles.negative
+                          }
+                        >
+                          {c['30d'].price_change_pct}
+                        </span>
+                      </div>
+                      <div className={styles.rightColumnItem}>
+                        Variation from 1 year:{' '}
+                        <span
+                          className={
+                            c['365d'].price_change_pct > 0
+                              ? styles.positive
+                              : styles.negative
+                          }
+                        >
+                          {c['365d'].price_change_pct}
+                        </span>
+                      </div>
+                    </div>
+                  </>
+                )}
+                <div
+                  className={styles.moreBttn}
+                  onClick={() => {
+                    handleToggle(index);
+                  }}
+                >
+                  {clicked == index ? 'Hide' : 'Details'}
+                </div>
+              </div>
+            ))}
+        </div>
       </ProtectedRoute>
     </JwtCookieProvider>
   );
